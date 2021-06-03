5076817325e688ccae062e73495061994f18f3a4
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-06-03 14:19:29 -0600

    Create Welcome.java

---
+++ Unit1-PrimitiveTypes/1.2.5/Welcome.java
@@ -0,0 +1,16 @@
+public class Welcome 
+{
+    public static void main(String[] args)
+    {
+        // Your code will go here
+        // These lines are comments
+        // Java ignores anything that comes after 
+        // two forward slashes
+        // You can delete the comments or leave them in
+        // Java ignores them anyway!
+        
+        
+        System.out.println("My name is Patryk.");
+        System.out.println("I like Python more than Java.");
+    } 
+}
