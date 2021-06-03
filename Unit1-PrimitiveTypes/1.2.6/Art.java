086cbf3cd0f004a94b1abe0aca776219a17ba9ef
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-06-03 14:20:12 -0600

    Create Art.java

---
+++ Unit1-PrimitiveTypes/1.2.6/Art.java
@@ -0,0 +1,12 @@
+public class Art 
+{
+    public static void main(String[] args)
+    {
+       //your code goes here!
+       System.out.println("   /)___");
+       System.out.println("  /     O|___");
+       System.out.println(" /           )");
+       System.out.println("/       )___/");
+    }
+
+}
