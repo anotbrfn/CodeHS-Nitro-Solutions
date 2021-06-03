928f3126d6ece6f4152fe7ad544ca406b3f374a9
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-06-03 14:21:39 -0600

    Create Timeline.java

---
+++ Unit1-PrimitiveTypes/Timeline.java
@@ -0,0 +1,29 @@
+public class Timeline
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        System.out.println("Age    | M/YY - Description");
+        System.out.println("7      | 7/07 - First trip to Disney World");
+        System.out.println("       |");
+        System.out.println("       |");
+        System.out.println("       |");
+        System.out.println("9      |");
+        System.out.println("       |");
+        System.out.println("       | 2/10 - Attended the Olympics in Vancouver");
+        System.out.println("       |");
+        System.out.println("11     | 7/11 - Attended first overnight camp");
+        System.out.println("       |");
+        System.out.println("       |");
+        System.out.println("       |");
+        System.out.println("13     |");
+        System.out.println("       | 9/13 - Started job delivering papers");
+        System.out.println("       |");
+        System.out.println("       | 11/14 - Won wrestling tournament");
+        System.out.println("15     |");
+        System.out.println("       | 8/15 - Started high school");
+        System.out.println("       |");
+        System.out.println("       |");
+        System.out.println("17     |");
+    }
+}
