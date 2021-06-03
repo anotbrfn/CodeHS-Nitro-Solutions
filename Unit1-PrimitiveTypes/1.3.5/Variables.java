6467edc3040e09cd9b239a2a9b596525885db306
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-06-03 14:22:10 -0600

    Create Variables.java

---
+++ Unit1-PrimitiveTypes/1.3.5/Variables.java
@@ -0,0 +1,10 @@
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        int currentYear = 2020;
+        
+        System.out.println("The current year is " + currentYear);
+    }
+}
