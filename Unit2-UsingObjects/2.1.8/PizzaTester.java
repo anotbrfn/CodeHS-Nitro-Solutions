78271fcac195167b5a5e0cf14248d06f64c505c2
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-06-04 17:42:46 -0600

    Create PizzaTester.java

---
+++ Unit2-UsingObjects/2.1.8/PizzaTester.java
@@ -0,0 +1,10 @@
+public class PizzaTester
+{
+    public static void main(String[] args)
+    {
+        System.out.println("You should be able to run this");
+        System.out.println("if you added the instance variables correctly");
+        
+        Pizza myPizza = new Pizza(true, "pepperoni", "mushrooms", "tomatoes", 16);
+    }
+}
