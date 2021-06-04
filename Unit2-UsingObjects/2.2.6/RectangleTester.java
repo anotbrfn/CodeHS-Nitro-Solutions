20c91d18063474d6d14484980f22cae23c1cf46a
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-06-04 17:44:05 -0600

    Create RectangleTester.java

---
+++ Unit2-UsingObjects/2.2.6/RectangleTester.java
@@ -0,0 +1,11 @@
+public class RectangleTester 
+{
+    public static void main(String[] args)
+    {
+        // Create a rectangle with width 5 and height 12
+        
+        // Then print it out
+        Rectangle myRectangle = new Rectangle(5, 12);
+        System.out.println(myRectangle);
+    }
+}
