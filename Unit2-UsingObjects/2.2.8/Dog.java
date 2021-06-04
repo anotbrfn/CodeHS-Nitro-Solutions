969c32888623515f5f83081b8194caa29f213f29
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-06-04 17:45:38 -0600

    Create Dog.java

---
+++ Unit2-UsingObjects/2.2.8/Dog.java
@@ -0,0 +1,20 @@
+public class Dog 
+{
+    private String breed;
+    // Add an instance variable here for name.
+    private String name;
+    
+    public Dog(String theBreed, String theName)
+    {
+        breed = theBreed;
+        name = theName;
+    }
+    
+    // This method should work after you add the
+    // new instance variable and update the constructor.
+    // DO NOT modify this method.
+    public String toString()
+    {
+        return name + " is a " + breed;
+    }
+}
