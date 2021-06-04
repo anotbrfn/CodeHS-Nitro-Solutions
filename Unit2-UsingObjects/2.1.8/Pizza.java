47621bfd628aead46040879fbaa258377d327a18
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-06-04 17:43:01 -0600

    Create Pizza.java

---
+++ Unit2-UsingObjects/2.1.8/Pizza.java
@@ -0,0 +1,17 @@
+public class Pizza
+{
+    // Add your instance variables here
+    boolean pizzaglutenfree;
+    String pizzatopping1;
+    String pizzatopping2;
+    String pizzatopping3;
+    int pizzadiameter;
+    
+    public Pizza(boolean glutenfree, String topping1, String topping2, String topping3, int diameter){
+        pizzaglutenfree = glutenfree;
+        pizzatopping1 = topping1;
+        pizzatopping2 = topping2;
+        pizzatopping3 = topping3;
+        pizzadiameter = diameter;
+    }
+}
