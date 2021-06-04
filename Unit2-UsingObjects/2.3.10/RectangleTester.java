95a1c59f4d8662e0d6cf68bf4a0749c1b320ef8d
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-06-04 17:52:34 -0600

    Create RectangleTester.java

---
+++ Unit2-UsingObjects/2.3.10/RectangleTester.java
@@ -0,0 +1,20 @@
+public class RectangleTester
+{
+    public static void main(String[] args)
+    {
+        // Fix this code so there are no more
+        // null pointer exceptions
+        Rectangle brick = new Rectangle(1,1);
+        Rectangle fencePost;
+        
+        Rectangle rect2 = new Rectangle(1, 3);
+        
+        System.out.println(brick);
+        System.out.println(rect2);
+        
+        // Change the fencePost dimensions
+        fencePost = new Rectangle(1, 5);
+        System.out.println(fencePost);
+        
+    }
+}
