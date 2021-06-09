b722a24351192996345a0a4123d221204adf9ad3
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-06-08 20:32:18 -0600

    Create Teen.java

---
+++ Unit4-Iteration/4.3.10/Teen.java
@@ -0,0 +1,45 @@
+public class Teen 
+{
+    private String firstName;
+    private String lastName;
+    private int grade;
+    private Boolean textMessages;
+
+    // Constructor to make a teen with a first and last name, grade in school, 
+    // and whether they text message others and need to write texts to others. 
+    
+    // This defines the state of the teen.
+    public Teen(String theFirstName, String theLastName, int theGrade, Boolean theTextMessages)
+    {
+        firstName = theFirstName;
+        lastName = theLastName;
+        grade = theGrade;
+        textMessages = theTextMessages;
+    }
+    
+    // toString method to print out the state of teen object
+    public String toString()
+    {
+        return firstName + " " + lastName + " is in grade " + grade + " and wants to send this text:";
+    }
+    
+    // Create this method so that it changes the text message 
+    // and places the word "like" in place of each space 
+    // in the message.
+    public String teenTalk(String text)
+    {
+        String last = text;
+        
+        for(int i = 0; i < last.length(); i++)
+        {
+            if(last.charAt(i) == ' ')
+            {
+                last = last.substring(0, i) + " like " + last.substring(i + 1);
+                i += 5;
+            }
+        }
+
+        return last;
+    }
+    
+}
