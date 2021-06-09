2bc594826a7d28080a86f90158daf800adf9dfe8
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-06-08 20:30:38 -0600

    Create MultiplicationTable.java

---
+++ Unit4-Iteration/4.2.10/MultiplicationTable.java
@@ -0,0 +1,11 @@
+public class MultiplicationTable
+{
+    public static void main(String[] args)
+    {
+        // Your code goes here!
+        for(int i = 1; i <= 10; i++){
+            int ans = 4 * i;
+            System.out.println("4 * " + i + " = " + ans);
+        }
+    }
+}
