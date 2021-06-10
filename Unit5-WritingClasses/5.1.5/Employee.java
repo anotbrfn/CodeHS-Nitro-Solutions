dd422b7221f21fc34aacc20934e1ebd88e4047cf
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-06-09 20:41:30 -0600

    Create Employee.java

---
+++ Unit5-WritingClasses/5.1.5/Employee.java
@@ -0,0 +1,19 @@
+public class Employee
+{
+    private String employee;
+    private int id;
+    private double salary;
+    
+    public Employee(String employeeName, int idNum, double salaryNum)
+    {
+        employee = employeeName;
+        id = idNum;
+        salary = salaryNum;
+    }
+    
+    
+    
+    
+    
+    
+}
