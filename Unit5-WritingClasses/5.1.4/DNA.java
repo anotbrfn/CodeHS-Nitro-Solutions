412e192afd23d33d06b93dc84cf680e378b9248c
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-06-09 20:41:00 -0600

    Create DNA.java

---
+++ Unit5-WritingClasses/5.1.4/DNA.java
@@ -0,0 +1,18 @@
+public class DNA
+{
+    private String rsid;
+    private String genotype;
+    
+    
+    public DNA (String rsidOne, String genotypeOne)
+    {
+        rsid = rsidOne;
+        genotype = genotypeOne;
+        
+    }
+    
+    
+    
+    
+    
+}
