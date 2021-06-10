07bfcea42dc88d14fc7721e7ff873cdcb6b0a2f0
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-06-09 20:44:10 -0600

    Create DogTester.java

---
+++ Unit5-WritingClasses/5.2.6/DogTester.java
@@ -0,0 +1,10 @@
+public class DogTester
+{
+    public static void main(String[] args)
+    {
+        Dog golden = new Dog("Perro", 10, 21.9);
+        Dog pug = new Dog("Paul", 5);
+        System.out.println(golden);
+        System.out.println(pug);
+    }
+}
