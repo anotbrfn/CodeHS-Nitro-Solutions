38b5d081c972ef17ba920ab290b734014df0646e
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-06-11 21:59:33 -0600

    Create Scope.java

---
+++ Unit5-WritingClasses/5.8.7/Scope.java
@@ -0,0 +1,43 @@
+public class Scope
+{
+    private int a;
+    private int b;
+    private int c;
+
+    public Scope(){
+        a = 5;
+        b = 10;
+        c = 15;
+    }
+
+    public void printScope(){
+       //Start here
+       System.out.println("a = " + getA());
+       System.out.println("b = " + getB());
+       System.out.println("c = " + getC());
+       System.out.println("d = " + getD());
+       System.out.println("e = " + getE());
+    }
+
+	public int getA() {
+		return a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public int getC() {
+		return c;
+	}
+
+    public int getD(){
+        int d = a + c;
+        return d;
+    }
+
+	public int getE() {
+        int e = b + c;
+		return e;
+	}
+}
