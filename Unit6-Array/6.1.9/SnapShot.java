64de61f5ef570636c021aeb57c1e318fdde18c45
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-06-12 22:00:39 -0600

    Update SnapShot.java

--- Unit6-Array/6.1.9/SnapShot.java
+++ Unit6-Array/6.1.9/SnapShot.java
@@ -1,1 +1,20 @@
-
+public class SnapShot
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        String[] arr = { "Welcome", "to", "the snap shot", "app!" };
+        for(int i = 0; i < arr.length; i++)
+        {
+            System.out.println(arr[i]);
+        }
+        System.out.println();
+        arr[0] = "Upgrade";
+        arr[arr.length - 1] = "premium app!";
+        
+        for(int i = 0; i < arr.length; i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
