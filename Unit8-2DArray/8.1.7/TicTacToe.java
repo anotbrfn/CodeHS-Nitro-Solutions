2ad39a98b4462d53c89596c88b54a074f13c8971
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-07-03 15:28:44 -0600

    Create TicTacToe.java

---
+++ Unit8-2DArray/8.1.7/TicTacToe.java
@@ -0,0 +1,16 @@
+public class TicTacToe
+{
+    private String[][] board;
+    public TicTacToe()
+    {
+        board = new String[3][3];
+        for(int i = 0; i < 3; i++)
+            for(int j = 0; j < 3; j++)
+                board[i][j] = "-";
+    }
+    public String[][] getBoard()
+    {
+        return board;
+    }
+    
+}
