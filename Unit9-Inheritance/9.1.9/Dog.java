e35c8ad560a89d071866d8744a5588999d5eea18
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-07-31 13:00:46 -0600

    Create Dog.java

---
+++ Unit9-Inheritance/9.1.9/Dog.java
@@ -0,0 +1,14 @@
+public class Dog extends Pet
+{
+   private boolean trained;
+   
+   public boolean isTrained()
+   {
+       return trained;
+   }
+   public void setTrain(boolean newTrained)
+   {
+       trained = newTrained;
+   }
+   
+}
