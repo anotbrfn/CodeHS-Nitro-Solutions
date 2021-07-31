8b99388a8a8d3f93c164c04e958706e60e626c82
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-07-31 12:59:49 -0600

    Create Laptop.java

---
+++ Unit9-Inheritance/9.1.8/Laptop.java
@@ -0,0 +1,13 @@
+public class Laptop extends Computer
+{
+   private double batteryLife;
+   
+   public double getBatteryLife()
+   {
+       return batteryLife;
+   }
+   public void setBatteryLife(double newlife)
+   {
+       batteryLife = newlife;
+   }
+}
