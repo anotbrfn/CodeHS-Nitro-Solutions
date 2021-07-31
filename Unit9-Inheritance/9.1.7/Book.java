2342a649b15ee625fa238375f536371844954830
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-07-31 12:58:45 -0600

    Create Book.java

---
+++ Unit9-Inheritance/9.1.7/Book.java
@@ -0,0 +1,21 @@
+public class Book
+{
+    private int pages;
+    private String name;
+
+    public int getPages() {
+        return pages;
+    }
+    
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
