0d3b91c2ef103f32ed0b992c7076f01d1f2d0878
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-07-31 13:00:29 -0600

    Create Animal.java

---
+++ Unit9-Inheritance/9.1.9/Animal.java
@@ -0,0 +1,14 @@
+public class Animal
+{
+   private String animal;
+   
+    public void setAnimal(String newAnimal)
+   {
+       animal = newAnimal;
+   }
+   public String getType()
+   {
+       return animal;
+   }
+   
+}
