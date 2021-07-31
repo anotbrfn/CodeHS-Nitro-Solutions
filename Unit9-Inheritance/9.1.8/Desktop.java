3c169a9b0ac2467f2ee0b2968df6f0aaa764d43a
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-07-31 12:59:39 -0600

    Create Desktop.java

---
+++ Unit9-Inheritance/9.1.8/Desktop.java
@@ -0,0 +1,15 @@
+public class Desktop extends Computer
+{
+    private boolean monitor;
+   
+   public boolean hasMonitor()
+   {
+       return monitor;
+   }
+   public void setMonitor(boolean bool)
+   {
+       monitor = bool;
+   }
+   
+    
+}
