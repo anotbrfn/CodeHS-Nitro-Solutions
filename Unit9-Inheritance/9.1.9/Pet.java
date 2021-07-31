dd5b0dc7f85753f42d3e3cca8a1fc9ba9353a1fe
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-07-31 13:00:18 -0600

    Create Pet.java

---
+++ Unit9-Inheritance/9.1.9/Pet.java
@@ -0,0 +1,23 @@
+public class Pet extends Animal
+{
+    private String name;
+    private String size;
+    
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String newName)
+    {
+        name = newName;
+    }
+    public String getSize()
+    {
+        return size;
+    }
+    public void setSize(String newSize)
+    {
+        size = newSize;
+    }
+   
+}
