fbe113028629c80b051f56ee791e87358b023ee4
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-07-31 13:04:04 -0600

    Create ClothingTester.java

---
+++ Unit9-Inheritance/9.2.9/ClothingTester.java
@@ -0,0 +1,16 @@
+  
+public class ClothingTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        TShirt a = new TShirt("L", "red", "denim");
+        Jeans b = new Jeans("S");
+        Sweatshirt c = new Sweatshirt("L", "green", true);
+        Jeans d = new Jeans("L");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
