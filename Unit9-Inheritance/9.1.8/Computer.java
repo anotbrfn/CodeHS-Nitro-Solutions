2334ca9de31f04ba26312549d110289977f526a5
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-07-31 12:59:28 -0600

    Create Computer.java

---
+++ Unit9-Inheritance/9.1.8/Computer.java
@@ -0,0 +1,26 @@
+public class Computer
+{
+    private int screenSize;
+    private int memory;
+    
+    public int getScreenSize()
+    {
+        return screenSize;
+    }
+    public void setScreenSize(int newSize)
+    {
+        screenSize = newSize;
+    }
+    
+    public int getMemory()
+    {
+        return memory;
+    }
+    public void setMemory(int newMemory)
+    {
+        memory = newMemory;
+    }
+    
+    
+    
+}
