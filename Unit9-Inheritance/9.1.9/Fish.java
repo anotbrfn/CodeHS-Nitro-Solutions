5e8e7611a91d93c754c5eafc7ccd674b6df4aad5
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-07-31 13:00:38 -0600

    Create Fish.java

---
+++ Unit9-Inheritance/9.1.9/Fish.java
@@ -0,0 +1,13 @@
+public class Fish extends Pet
+{
+    private String water;
+    public String getType()
+    {
+        return water;
+    }
+    public void setWater(String newWater)
+    {
+        water = newWater;
+    }
+    
+}
