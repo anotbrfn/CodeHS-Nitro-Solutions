752a396b265bbf6705a12604e0f91d05e7f6983b
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-08-08 21:39:49 -0600

    Create LoudDog.java

---
+++ Unit9-Inheritance/9.3.6/LoudDog.java
@@ -0,0 +1,19 @@
+public class LoudDog extends Dog
+{
+   
+   public LoudDog(String name){
+       super(name);
+   }
+   
+   // Override the speak method here
+   public String speak(){
+       return "BARK!";
+   }
+   
+   
+   //Override the toString here.
+   //Remember, you can access the name using super.getName()
+   public String toString(){
+       return super.getName() + " is loud and likes to " + speak();
+   }
+}
