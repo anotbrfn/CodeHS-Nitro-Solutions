81ebcf0d792e6b4e59c02e8ab60a2b199377eecd
Patrick Dobranowski <patrick.dobranowski@gmail.com>
2021-08-08 21:39:25 -0600

    Create DogTester.java

---
+++ Unit9-Inheritance/9.3.6/DogTester.java
@@ -0,0 +1,10 @@
+public class DogTester
+{
+    public static void main(String[] args)
+    {
+        // Start here
+        Dog a = new Dog("a");
+        LoudDog b = new LoudDog("b");
+        System.out.println(a + "\n" + b);
+    }
+}
